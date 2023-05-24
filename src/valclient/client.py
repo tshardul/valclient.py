74ecd66d265a25740bfbb8d6be520f2947524740
tshardul <31133786+tshardul@users.noreply.github.com>
2023-05-24 19:14:11 +0200

    Update client.py

--- src/valclient/client.py
+++ src/valclient/client.py
@@ -1029,7 +1029,7 @@ class Client:
         Get player username from PUUID
         """
         puuid = self.__check_puuid(puuid)
-        data = self.fetch(endpoint="/name-service/v2/players", endpoint_type="local")
+        data = self.fetch(endpoint="/name-service/v2/players", endpoint_type="pd")
         return data
 
 
